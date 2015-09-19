4211ad32410e49c9b9f96c115cb1ceeb50ed9a0e
femtotrader <femto.trader@gmail.com>
2015-09-19 16:46:31 +0200

    Add utils.py

---
+++ scripts/utils.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+def get_user_project(url):
+    """
+    Returns a tuple (user, project) from a GitHub URL
+    """
+    pattern = "https:\/\/github.com\/(.*)\/(.*)"
+    m = re.match(pattern, url)
+    if m is not None:
+        (user, project) = m.groups()
+        return user, project
+    return '', ''
+
+def build_github_url(user, project, branch='master'):
+    """
+    Returns a GitHub URL from user, project and branch
+    branch can be, for example 'master' but it can also be 'gh-pages'
+    """
+    return "https://raw.githubusercontent.com/%s/%s/%s/" % (user, project, branch)
+
+
+def request_dpkg(url, session):
+    """
+    Returns response when querying a datapackage
+    """
+    return session.get(url + 'datapackage.json')
+
+def is_valid(url, session):
+    """
+    Return tuple (valid, errors) to know if url is a valid DataPackage
+    """
+    url_api_base = 'http://data.okfn.org'
+    endpoint = '/tools/validate.json'
+    try:
+        response = session.get(url_api_base + endpoint, params={'url': url})
+        resp = response.json()
+        return resp['valid'], resp['errors']
+    except:
+        return False, traceback.format_exc()
